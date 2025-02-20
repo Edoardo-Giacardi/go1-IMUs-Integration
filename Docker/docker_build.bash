7c63242c3bc00c9634ab31dd6ef07fd027033af8
Edoardo-Giacardi <edoardogiaca2001@gmail.com>
2025-02-20 18:25:56 +0100

    Changed the docker folder

--- Docker/docker_build.bash
+++ Docker/docker_build.bash
@@ -1,28 +1,25 @@
-#!/usr/bin/env bash
-
-# ===========================================
-# docker_build.bash
-# ===========================================
-# This script defines build arguments and
-# builds the Docker image with a single
-# command.
-# ===========================================
+#!/bin/bash
+# make the file executable: chmod +x docker_build.bash
+# ================================= Edit Here ================================ #
 
 # -- Build Arguments --
-USERNAME="ros"
-USER_UID="1000"
-USER_GID="1000"
-WORKSPACE="ros2_ws"
-IMAGE="ros2_mipo"
-TAG="latest"
+USERNAME=ros
+USER_UID=1000
+USER_GID=1000
+WORKSPACE=ros2_ws
+IMAGE=ros2_mipo
+TAG=latest
 
-# -- Docker Build --
+
+
+# =============================== BUILD ============================== #
+
 docker build \
-    --build-arg USERNAME="${USERNAME}" \
-    --build-arg USER_UID="${USER_UID}" \
-    --build-arg USER_GID="${USER_GID}" \
-    --build-arg WORKSPACE="${WORKSPACE}" \
-    --build-arg IMAGE="${IMAGE}" \
-    --build-arg TAG="${TAG}" \
-    -t "${IMAGE}:${TAG}" \
+    --build-arg USERNAME=$USERNAME \
+    --build-arg USER_UID=$USER_UID \
+    --build-arg USER_GID=$USER_GID \
+    --build-arg WORKSPACE=$WORKSPACE \
+    --build-arg IMAGE=$IMAGE \
+    --build-arg TAG=$TAG \
+    -t $IMAGE:$TAG \
     .
